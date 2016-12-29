174085528307e97800703c6879d946350d092dae
Ranmocy Sheng <ranmocy@gmail.com>
2016-12-29 09:01:17 -0800

    setup basic android test

--- app/src/androidTest/java/me/ranmocy/monkeytree/MonkeyActivityTest.java
+++ app/src/androidTest/java/me/ranmocy/monkeytree/MonkeyActivityTest.java
@@ -7,15 +7,13 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Android test for {@link MonkeyActivity}.
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MonkeyActivityTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
